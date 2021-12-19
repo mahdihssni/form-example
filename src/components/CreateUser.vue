7beb37f2939444a38b466f8d82fc0870bca89281
Mahdi Hasani <mahdi.hasani@arvancloud.com>
2021-12-19 13:57:40 +0330

    create user storage converted to vuex

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -6,31 +6,17 @@
             <v-container>
                 <v-row>
                     <v-col md="6">
-                        <v-text-field
-                            v-model="newUser.name"
-                            outlined
-                            dense
-                            label="Name"
-                            required
-                        />
+                        <v-text-field v-model="newUser.name" outlined dense label="Name" required />
                     </v-col>
                     <v-col md="6">
-                        <v-text-field
-                            v-model="newUser.email"
-                            outlined
-                            dense
-                            label="Email"
-                            required
-                        />
+                        <v-text-field v-model="newUser.email" outlined dense label="Email" required />
                     </v-col>
                 </v-row>
             </v-container>
 
             <v-card-actions>
                 <v-btn @click="onSubmit" color="primary">Create</v-btn>
-                <v-btn @click="createDialog = false" color="secondary">
-                    Close
-                </v-btn>
+                <v-btn @click="createDialog = false" color="secondary">Close</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
@@ -38,12 +24,6 @@
 
 <script>
 export default {
-    props: {
-        open: {
-            type: Boolean,
-            required: true,
-        },
-    },
     data() {
         return {
             newUser: {
@@ -56,7 +36,7 @@ export default {
     computed: {
         createDialog: {
             get() {
-                return this.$props.open;
+                return this.$store.getters.getDialogStatus("create");
             },
             set(value) {
                 if (!value) {
@@ -64,15 +44,16 @@ export default {
                     this.newUser.email = "";
                 }
 
-                this.$emit("createDialog", value);
+                this.$store.commit("dialogTrigger", { dialogName: "create", isOpen: value });
             },
         },
     },
     methods: {
         onSubmit() {
-            this.newUser.id = this.uid
-            this.$emit("create", this.newUser)
-        }
-    }
+            this.newUser.id = this.uid;
+            this.$store.commit('addUser', this.newUser)
+            this.$store.commit("dialogTrigger", { dialogName: "create", isOpen: false });
+        },
+    },
 };
 </script>
--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,31 +1,52 @@
 import { getData } from "../../utils/data";
 
+const dialogTriggerEnums = {
+    create: "isCreateDialogOpen",
+    edit: "isEditDialogOpen",
+    delete: "isDeleteDialogOpen",
+};
+
 const state = () => ({
     list: getData(100),
     usersPerPage: 15,
-    currentPage: 1
-})
+    currentPage: 1,
+    isCreateDialogOpen: false,
+    isDeleteDialogOpen: false,
+    isEditDialogOpen: false,
+    selectedUser: null,
+});
 
 const getters = {
     usersPaginatedList(state) {
-        return state.list.slice(
-            (state.currentPage - 1) * state.usersPerPage,
-            state.currentPage * state.usersPerPage
-        );
+        return state.list.slice((state.currentPage - 1) * state.usersPerPage, state.currentPage * state.usersPerPage);
     },
     totalPages(state) {
         return Math.ceil(state.list.length / state.usersPerPage);
-    }
-}
+    },
+    getDialogStatus(state) {
+        return (dialogName) => {
+            return state[dialogTriggerEnums[dialogName]];
+        };
+    },
+};
 
 const mutations = {
     changeCurrentPage(state, pageNumber) {
-        state.currentPage = pageNumber
-    }
-}
+        state.currentPage = pageNumber;
+    },
+    dialogTrigger(state, options) {
+        let { dialogName, isOpen, users } = options;
+
+        state[dialogTriggerEnums[dialogName]] = isOpen;
+        state.selectedUser = ["edit", "delete"].includes(dialogName) && users ? users : null;
+    },
+    addUser(state, user) {
+        state.list.unshift(user);
+    },
+};
 
 export default {
     state,
     getters,
-    mutations
-}
+    mutations,
+};
